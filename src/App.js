6e08cd4369f02cf6a0877d3fdb73b7873956ba28
Haley Warson <haleywarson@gmail.com>
2021-04-12 11:00:57 -0600

    Updated app with initial routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route,Switch} from 'react-router-dom'
+import Root from './Root'
+import SignIn from './SignIn'
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header>HI</header>
+        <Switch>
+          <Route path="/signin">
+            <SignIn />
+          </Route>
+          <Route path="/">
+            <Root />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
+
 }
 
 export default App;
